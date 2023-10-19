1718cf41b28d91ee393ba738eb52f9cf72a06e34
pad <pad@maitrebitcoin.com>
2023-10-19 09:03:30 +0200

    adding grammar

--- bitVM-compiler/Compiler.cpp
+++ bitVM-compiler/Compiler.cpp
@@ -5,8 +5,12 @@
 #include <vector>
 #include <assert.h>
 #include <regex>
+#include <functional>
 
-
+// constructor
+Compiler::Compiler(void) {
+	_init_grammar();
+}
 
 
 // type of a variable or function
@@ -24,6 +28,9 @@ public:
 protected:
 	Native native_type = Native::undefined;
 public:
+	// constructor
+	Type(Native t): native_type(t) {}
+
 	// return the size in bits of the type
 	int size_in_bit(void) const;
 
@@ -87,16 +94,20 @@ protected:
 
 // represents a function, ex : bool main(bool a, bool b) { return a & b; }
 class Function {
+public:
+	// parameters of the function
+	struct Parameter {
+		Type type;
+		std::string name;
+		// constructor
+		Parameter(Type t, std::string n) : type(t), name(n) {}
+	};
 protected:
 	// name of the function
 	std::string name;
 	// return type of the function
 	Type return_type;
-	// parameters of the function
-	struct Parameter {
-		Type type;
-		std::string name;
-	};
+
 	std::vector<Parameter> parameters;
 
 public:
@@ -153,11 +164,12 @@ bool Compiler::compile(std::istream& source_code, Error& error_out)
 
 
 struct TokenDefinition {
-	int token_type;
+	TokenType   token_type;
 	const char* token_value;
 	const char* regex;
 };
 
+// init token definition (lexer)
 #define TOKEN_TYPE_BOOL  257
 #define TOKEN_TYPE_BYTE  258
 #define TOKEN_IDENTIFIER 258
@@ -169,6 +181,93 @@ TokenDefinition token_definition[] =
 	{ TOKEN_IDENTIFIER, nullptr, "[a-zA-a_][a-zA-a0-9_]*"},
 	{ TOKEN_NUMBER,		nullptr, "[0-9]*"},
 };
+
+// init grammar
+#define RULE_FUNCTION			 1000
+#define RULE_TYPE				 1001  // ex : bool
+#define RULE_N_PARAMETERS_DECL	 1002  // ex : (bool a, byte b)
+#define RULE_1_PARAMETER_DECL	 1003  // ex : bool a 
+#define RULE_CODEBLOC			 1004  // ex : { return a+b; }
+#define RULE_N_STATEMENTS		 1005
+#define RULE_1_STATEMENT		 1006
+#define RULE_OPERATION			 1007
+#define RULE_INSTRUCITON_RETURN	 1100
+
+void Compiler::_init_grammar(void) 
+{
+	RuleDefinition rules_definition[] =
+	{
+		// ex : bool fct_name(byte a, byte b) { return a+b; }
+		{ RULE_FUNCTION, { RULE_TYPE, TOKEN_IDENTIFIER, '(', RULE_N_PARAMETERS_DECL ,')', RULE_CODEBLOC }, nullptr },
+		// ex : (bool a, byte b)
+		{ RULE_N_PARAMETERS_DECL , { RULE_N_PARAMETERS_DECL, ',', RULE_1_PARAMETER_DECL } , nullptr },
+		{ RULE_N_PARAMETERS_DECL , { RULE_1_PARAMETER_DECL }  , nullptr  },
+		// ex : bool a 
+		{ RULE_1_PARAMETER_DECL , { RULE_TYPE, TOKEN_IDENTIFIER } ,   
+			[this](TokenValue& result, std::vector<TokenValue> param) { result.function_paramter_value = new Function::Parameter(*param[0].type_value, *param[1].string_value);  }
+		},
+		// ex : bool
+		{ RULE_TYPE , { TOKEN_TYPE_BOOL } , 
+			[this](TokenValue& result, std::vector<TokenValue> param) { result.type_value = new Type(Type::Native::bit);  }
+		},
+		//{ RULE_TYPE , { TOKEN_TYPE_BYTE } , nullptr  },
+	};
+	int nb_rules = sizeof(rules_definition) / sizeof(rules_definition[0]);
+	// init the grammar array
+	grammar_rules = std::vector<RuleDefinition>(rules_definition, rules_definition + nb_rules);
+
+}
+
+// test if a rule is matching the current stack
+bool Compiler::is_rule_matching_stack(RuleDefinition& rule) const {
+	// check if the stack is big enough
+	size_t rule_size = rule.tokens.size();
+	if (token_stack.size()< rule_size)
+		return false;
+	// check if the rule match the top of the stack
+	size_t stack_top = token_stack.size() - rule_size;
+	for (int i=0; i< rule_size; i++) {
+		if (rule.tokens[i] != token_stack[stack_top+i].type)
+			return false;
+	}
+	// matching ok
+	return true;
+}	
+
+// find a matching rule from the token stack
+RuleDefinition* Compiler::_find_matching_rule_from_stack(void)  {
+	// for each rule in the grammar
+	for ( RuleDefinition& rule : grammar_rules) {
+		// check if the rule match the token stack
+		if (is_rule_matching_stack(rule))
+			return &rule;
+	}
+	// no rule found
+	return nullptr;
+}
+// call a rule and remove the token from the stack
+void Compiler::_execute_rule(RuleDefinition& rule) {
+	size_t rule_size = rule.tokens.size();
+	size_t stack_top = token_stack.size() - rule_size;
+	assert(stack_top >= 0);
+	// get values form the stack
+	std::vector<TokenValue> values;
+	for (int i = 0; i < rule_size; i++) {
+		values.push_back(token_stack[stack_top + i].value);
+	}
+	// call the rule to set token_result.value 
+	CToken token_result(rule.rule_id);
+	if (rule.action != nullptr)
+		rule.action(token_result.value, values);
+
+	// remove the token from the top of the stack
+	for (int i = 0; i < rule_size; i++) {
+		token_stack.pop_back();
+	}
+	// push the result on the stack
+	token_stack.push_back(token_result);
+
+}
 
 
 // get 1 token from the line
@@ -199,12 +298,12 @@ CToken Compiler::_get_next_token(std::string& code_in_out) {
 			assert(definition.regex != nullptr);
 			//  check if the string start with the token with definition.regex
 			std::string regex_definition = definition.regex;
-			std::regex regex_i( regex_definition );
+			std::regex regex_i(regex_definition);
 			std::smatch regex_match;
-			if (std::regex_search(code_in_out , regex_match, regex_i)) {
+			if (std::regex_search(code_in_out, regex_match, regex_i)) {
 				std::string match_string = regex_match[0];
 				// check if the string start with the matched part in the regexp 
-				if (match_string.size()>0 && code_in_out.compare(0, match_string.size(), match_string) == 0) {
+				if (match_string.size() > 0 && code_in_out.compare(0, match_string.size(), match_string) == 0) {
 					// remove the token from the string
 					code_in_out.erase(0, match_string.size());
 					// return the token
@@ -221,6 +320,7 @@ CToken Compiler::_get_next_token(std::string& code_in_out) {
 	return CToken(c, tok_c);
 }
 
+
 // compile 1 line of code
 void Compiler::_compile_line(std::string line_of_code) {
 	// ignore empty lines
@@ -236,7 +336,14 @@ void Compiler::_compile_line(std::string line_of_code) {
 			return;
 		// add to stack	
 		token_stack.push_back(token);
-
+		// check if we have a matching rule
+		RuleDefinition* rule = _find_matching_rule_from_stack();
+		while (rule != nullptr) {
+			// call the rule, remove the token from the stack and push the result
+			_execute_rule(*rule);
+			// do it utile the no more match
+			rule = _find_matching_rule_from_stack();
+		}
 
 	}
 
--- bitVM-compiler/Compiler.h
+++ bitVM-compiler/Compiler.h
@@ -4,17 +4,39 @@
 
 #include <string>
 #include <vector>
+#include <functional>
+
+using TokenType = int;
+//using TokenValue = std::string;
+union TokenValue {
+	std::string *string_value;
+	class Type* type_value;
+	class FunctionParameter* function_paramter_value;
+};
 
 class CToken {
 public:
 	// type of the token. ex '=' or TOKEN_TYPE_BOOL
-	int type;
+	TokenType type;
 	// value of the token. ex "bool"
-	std::string value;
+	std::string value_buffer; // point value if type if a "string_value"
+	TokenValue value;
 public:
-	// constructor
-	CToken(int t, std::string v) : type(t), value(v) {}
+	// constructors
+	CToken(int t) : type(t) {}
+	CToken(int t, std::string v) : type(t)
+	{
+		value_buffer = v;
+		value.string_value = &value_buffer;
+	}
 
+};
+class RuleDefinition {
+public:
+	TokenType rule_id;
+	std::vector<TokenType> tokens;
+	// callback function if the rule is matched
+	std::function<void (TokenValue& result, std::vector<TokenValue> values)> action;
 };
 
 class Compiler
@@ -32,17 +54,29 @@ public:
 		Error(const char* mess) : message(mess) {}
 	};
 protected:
+	// grammar of the compiler 
+	std::vector <RuleDefinition> grammar_rules;
+
 	// tokenss waiting to be processed by the compiler
 	std::vector<CToken> token_stack;
 public:
+	// constructor
+	Compiler(void);
 	// compile a memory stream
 	bool compile(std::istream& source_code, Error& error_out);
 protected:
+	// init the grammar
+	void _init_grammar(void);
 	// compile 1 line of code
 	void _compile_line(std::string line_of_code);
 	// get 1 token from code
 	CToken _get_next_token(std::string& code_in_out);
-
+	// test if a rule is matching the current stack
+	bool is_rule_matching_stack(RuleDefinition& rule) const;
+	// find a matchin rule from the token stack
+	RuleDefinition* _find_matching_rule_from_stack(void);
+	// call a rule and remove the token from the stack
+	void _execute_rule(RuleDefinition& rule);
 };
 
 
