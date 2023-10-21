eb4eff1229bc11b1542e7aea1b6ed740d461c816
pad <pad@maitrebitcoin.com>
2023-10-21 08:12:26 +0200

    compiler : support of R conditions

--- bitVM-compiler/Compiler.cpp
+++ bitVM-compiler/Compiler.cpp
@@ -14,30 +14,37 @@ Compiler::Compiler(void) {
 
 
 // compile a memory stream
-bool Compiler::compile(std::istream& source_code, Error& error_out)
+bool Compiler::compile(std::istream& source_code_stream, Error& error_out)
 {
+	// init the lexer
+	lexer.init(source_code_stream);
+
 	// read the source code
-	static const int max_line_size = 1024;
-	char line_raw[max_line_size];
-	int cureent_line_number = 0;
-	while (source_code.getline(line_raw, max_line_size)) {
+	int current_line_number = 0;
+	while (lexer.read_line()) {
 		// compile 1 line
-		std::string source_code_at_line(line_raw);
-		try {
-			_compile_line(source_code_at_line);
-		}
-		catch (Error& compiler_error)	{
+		Result result =	_compile_line();
+		switch (result)
+		{	
+		case Result::nextLine:
+			// continue compilation
+			break;
+		case Result::syntaxError:
 			// compilation failed
-			error_out = compiler_error;
-			error_out.message += " : " + source_code_at_line; // std::to_string(cureent_line_number);
-			error_out.line_number = cureent_line_number;
+			error_out.message     = "syntax error : " + lexer.get_source_code_current_line(); 
+			error_out.line_number = current_line_number;
 			return false;
+		case Result::endOfCode:
+			// compilation success
+			return true;
 		}
 		// next line
-		cureent_line_number++;
+		current_line_number++;
 	}
-	// OK
-	return true;
+	// compilation failed
+	error_out.message = "syntax error : unexpected end of code";
+	error_out.line_number = current_line_number;
+	return false;
 }
 
 
@@ -61,6 +68,18 @@ TokenDefinition token_definition[] =
 };
 
 // init grammar
+enum RuleType {
+	RULE_FUNCTION				= 1000,
+	RULE_TYPE,							// ex : bool
+	RULE_N_PARAMETERS_DECL,				// ex : (bool a, byte b)		
+	RULE_1_PARAMETER_DECL,				// ex : { return a+b; }
+	RULE_CODEBLOC,
+	RULE_N_STATEMENTS,
+	RULE_1_STATEMENT,
+	RULE_OPERATION,
+	RULE_INSTRUCITON_RETURN,
+};
+/*
 #define RULE_FUNCTION			 1000
 #define RULE_TYPE				 1001  // ex : bool
 #define RULE_N_PARAMETERS_DECL	 1002  // ex : (bool a, byte b)
@@ -70,6 +89,7 @@ TokenDefinition token_definition[] =
 #define RULE_1_STATEMENT		 1006
 #define RULE_OPERATION			 1007
 #define RULE_INSTRUCITON_RETURN	 1100
+*/
 
 void Compiler::_init_grammar(void) 
 {
@@ -78,6 +98,7 @@ void Compiler::_init_grammar(void)
 		// ex : bool fct_name(byte a, byte b) { return a+b; }
 		{ RULE_FUNCTION, { RULE_TYPE, TOKEN_IDENTIFIER, '(', RULE_N_PARAMETERS_DECL ,')', RULE_CODEBLOC }, nullptr },
 		// ex : (bool a, byte b)
+
 		{ RULE_N_PARAMETERS_DECL , { RULE_N_PARAMETERS_DECL, ',', RULE_1_PARAMETER_DECL } , nullptr },
 		{ RULE_N_PARAMETERS_DECL , { RULE_1_PARAMETER_DECL }  , nullptr  },
 		// ex : bool a 
@@ -91,13 +112,64 @@ void Compiler::_init_grammar(void)
 		//{ RULE_TYPE , { TOKEN_TYPE_BYTE } , nullptr  },
 	};
 	int nb_rules = sizeof(rules_definition) / sizeof(rules_definition[0]);
+
 	// init the grammar array
-	grammar_rules = std::vector<RuleDefinition>(rules_definition, rules_definition + nb_rules);
+	for (int i = 0; i < nb_rules; i++) {
+		GrammarRule grammar_rule_i(rules_definition[i]);
+		grammar_rules.push_back(grammar_rule_i);
+	}
+	// init map to rules
+	for (GrammarRule& rule : grammar_rules)	{
+		rules_map[rule.rule_id].push_back( & rule );
+	}
+
+
+	// 1sr rule is the start rule
+	grammar_rules[0].is_root = true;
+
+	// init the token array
+	for (GrammarRule& a_rule : grammar_rules)
+	{
+		// if only 1 token
+		if (a_rule.tokens.size() == 1) {
+			// if a a terminal leaf
+			a_rule.is_terminal = true;
+			continue;
+		}
+		// init right_token_conditions
+		for (int i = 0; i < a_rule.tokens.size() - 1; i++) {
+			//  token & next token
+			// ex :  RULE_TYPE, TOKEN_IDENTIFIER
+			TokenType token_i	  = a_rule.tokens[i ];
+			TokenType right_token = a_rule.tokens[i + 1];
+			// if token i is another rule
+			_init_grammar_right_conditions(token_i, right_token);
+		}
+	}
+
 
 }
+// init right conditions (récursive)
+void Compiler::_init_grammar_right_conditions(TokenType token, TokenType right_token_required){
+
+	//  only if token is another rule
+	if (rules_map.count(token) == 0)
+		return;
+	for (GrammarRule* sub_rule_i : rules_map[token])
+	{
+		// if <right_token_required> is not alredy present
+		std::vector<TokenType>& R_conditions= sub_rule_i->right_token_conditions;
+		bool is_already_present = std::find(R_conditions.begin(), R_conditions.end(), right_token_required) != R_conditions.end();
+		if (!is_already_present)
+			sub_rule_i->right_token_conditions.push_back(right_token_required);
+		// récursive for the rightmost rule
+		_init_grammar_right_conditions(sub_rule_i->tokens.back(), right_token_required);
+	}
+}
+
 
 // test if a rule is matching the current stack
-bool Compiler::is_rule_matching_stack(RuleDefinition& rule) const {
+bool Compiler::is_rule_matching_stack(GrammarRule& rule)  {
 	// check if the stack is big enough
 	size_t rule_size = rule.tokens.size();
 	if (token_stack.size()< rule_size)
@@ -108,14 +180,31 @@ bool Compiler::is_rule_matching_stack(RuleDefinition& rule) const {
 		if (rule.tokens[i] != token_stack[stack_top+i].type)
 			return false;
 	}
-	// matching ok
-	return true;
+	// terminal rule
+	if (rule.is_terminal )
+		// matching ok
+		return true;
+	// no post condition .
+	if (rule.right_token_conditions.size()==0)
+		// matching ok
+		return true;
+
+	// Read one token after the rule
+	CToken next_token = lexer.get_next_token( CLexer::ReadOption::keep );
+	// chekc if if match the post condition
+	for (TokenType right_token : rule.right_token_conditions) {
+		if (right_token == next_token.type)
+			return true; // one R condition is matching
+	}
+	// no R condition is matching
+	return false;
+
 }	
 
 // find a matching rule from the token stack
-RuleDefinition* Compiler::_find_matching_rule_from_stack(void)  {
+GrammarRule* Compiler::_find_matching_rule_from_stack(void)  {
 	// for each rule in the grammar
-	for ( RuleDefinition& rule : grammar_rules) {
+	for (GrammarRule& rule : grammar_rules) {
 		// check if the rule match the token stack
 		if (is_rule_matching_stack(rule))
 			return &rule;
@@ -124,7 +213,7 @@ RuleDefinition* Compiler::_find_matching_rule_from_stack(void)  {
 	return nullptr;
 }
 // call a rule and remove the token from the stack
-void Compiler::_execute_rule(RuleDefinition& rule) {
+void Compiler::_execute_rule(GrammarRule& rule) {
 	size_t rule_size = rule.tokens.size();
 	size_t stack_top = token_stack.size() - rule_size;
 	assert(stack_top >= 0);
@@ -147,9 +236,36 @@ void Compiler::_execute_rule(RuleDefinition& rule) {
 
 }
 
+// Init lexer
+void CLexer::init(std::istream& source) {
+	source_code = &source;
+}
+
+// read one line of code
+bool CLexer::read_line(void) {
+	static const int max_line_size = 1024;
+	char line_raw[max_line_size];
+	int cureent_line_number = 0;
+	if (!source_code->getline(line_raw, max_line_size))
+		return false;
+	std::string line(line_raw);
+	source_code_current_line = line;
+	remaining_ligne          = line;
+	// ok
+	return true;
+}
+
 
 // get 1 token from the line
-CToken Compiler::_get_next_token(std::string& code_in_out) {
+CToken CLexer::get_next_token(ReadOption option) {
+	// working string
+	std::string &code_in_out = remaining_ligne;
+	std::string temp;	
+	if (option == ReadOption::keep) {
+		// work on a copy
+		temp = remaining_ligne;
+		code_in_out = temp;
+	}
 
 	// ignore white space and tab
 	while (code_in_out[0] == ' ' || code_in_out[0] == '\t')
@@ -166,7 +282,8 @@ CToken Compiler::_get_next_token(std::string& code_in_out) {
 			// check if the string start with the token
 			if (code_in_out.compare(0, strlen(definition.token_value), definition.token_value) == 0) {
 				// remove the token from the string
-				code_in_out.erase(0, strlen(definition.token_value));
+				if (option == ReadOption::remove)
+					code_in_out.erase(0, strlen(definition.token_value));
 				// return the token
 				return CToken(definition.token_type, definition.token_value);
 			}
@@ -183,7 +300,8 @@ CToken Compiler::_get_next_token(std::string& code_in_out) {
 				// check if the string start with the matched part in the regexp 
 				if (match_string.size() > 0 && code_in_out.compare(0, match_string.size(), match_string) == 0) {
 					// remove the token from the string
-					code_in_out.erase(0, match_string.size());
+					if (option == ReadOption::remove)
+						code_in_out.erase(0, match_string.size());
 					// return the token
 					return CToken(definition.token_type, match_string);
 				}
@@ -194,38 +312,45 @@ CToken Compiler::_get_next_token(std::string& code_in_out) {
 	// send the first char as a token
 	char c = code_in_out[0];
 	char tok_c[] = { c,0 };
-	code_in_out.erase(0, 1);
+	if (option == ReadOption::remove)
+		code_in_out.erase(0, 1);
 	return CToken(c, tok_c);
 }
 
 
 // compile 1 line of code
-void Compiler::_compile_line(std::string line_of_code) {
+Compiler::Result Compiler::_compile_line(void) {
 	// ignore empty lines
-	if (line_of_code.size() == 0) return;
-
-
+	if (lexer.is_current_line_empty()) 
+		return Result::nextLine;;
 
 	// read all the tokens
 	while (1) {
 		// get a token for the line
-		CToken token = _get_next_token(line_of_code);
+		CToken token = lexer.get_next_token( CLexer::ReadOption::remove);
 		if (token.type == 0)
-			return;
+			return Result::nextLine; // End of line, read next line
+		if (token.type == INVALID_TOKEN)
+			return Result::syntaxError;
 		// add to stack	
 		token_stack.push_back(token);
 		// check if we have a matching rule
-		RuleDefinition* rule = _find_matching_rule_from_stack();
+		GrammarRule* rule = _find_matching_rule_from_stack();
 		while (rule != nullptr) {
 			// call the rule, remove the token from the stack and push the result
 			_execute_rule(*rule);
 			// do it utile the no more match
 			rule = _find_matching_rule_from_stack();
 		}
-
+		// if found the finale rule
+		if (rule && rule->is_terminal)
+		{
+			// check if the stack is empty
+			if (token_stack.size()>1)
+				return Result::syntaxError;
+			// sucess
+			return Result::endOfCode;
+		}
 	}
 
-		
-	// TODO : parse the line
-	throw Error("syntax error");
 }
--- bitVM-compiler/Compiler.h
+++ bitVM-compiler/Compiler.h
@@ -5,10 +5,11 @@
 #include <string>
 #include <vector>
 #include <functional>
+#include <map>
 #include "BtcContract.h"
 
 using TokenType = int;
-
+static const TokenType INVALID_TOKEN  = - 1;
 
 //using TokenValue = std::string;
 union TokenValue {
@@ -36,12 +37,56 @@ public:
 	}
 
 };
-class RuleDefinition {
-public:
-	TokenType rule_id;
+enum RuleType;
+struct RuleDefinition {
+	// rule to produce
+	RuleType rule_id;
+	// conditions to produce the rules
 	std::vector<TokenType> tokens;
 	// callback function if the rule is matched
 	std::function<void (TokenValue& result, std::vector<TokenValue> values)> action;
+};
+class GrammarRule : public RuleDefinition  {
+public:
+	bool is_root	 = false;
+	bool is_terminal = false;
+	// LR(1) parsing :
+	// right conditions to produce the rules.  empty  = all is valid
+	std::vector<TokenType> right_token_conditions;
+
+public:
+	// constructor
+	GrammarRule(RuleDefinition definition) {
+		rule_id = definition.rule_id;
+		tokens  = definition.tokens;
+		action  = definition.action;
+	}
+};
+
+class CLexer {
+protected:
+	// all source code
+	std::istream* source_code=nullptr;
+	// current line code
+	std::string source_code_current_line;
+	// what remains to be read in current line
+	std::string remaining_ligne;
+public:
+	// Init
+	void init(std::istream& source);
+
+	// read one line of code
+	bool read_line(void);
+	// is the line empty ?
+	bool is_current_line_empty(void) const { return source_code_current_line.size() == 0; }
+	// get 1 token from code
+	enum class ReadOption {
+		remove,
+		keep
+	};
+	CToken get_next_token(ReadOption option);
+	// get le this of code (for error messages)
+	std::string get_source_code_current_line(void) const { return source_code_current_line; }
 };
 
 class Compiler
@@ -59,8 +104,13 @@ public:
 		Error(const char* mess) : message(mess) {}
 	};
 protected:
+	// token lexer : get tokens from the code
+	CLexer lexer;
+
 	// grammar of the compiler 
-	std::vector <RuleDefinition> grammar_rules;
+	std::vector <GrammarRule> grammar_rules;
+	// find rules from its id
+	std::map< TokenType, std::vector<GrammarRule*>> rules_map;
 
 	// tokenss waiting to be processed by the compiler
 	std::vector<CToken> token_stack;
@@ -72,16 +122,22 @@ public:
 protected:
 	// init the grammar
 	void _init_grammar(void);
+	// initgrammar : right conditions (récursive)
+	void _init_grammar_right_conditions(TokenType token, TokenType right_token_required);
+
 	// compile 1 line of code
-	void _compile_line(std::string line_of_code);
-	// get 1 token from code
-	CToken _get_next_token(std::string& code_in_out);
+	enum class Result {
+		nextLine,
+		syntaxError,
+		endOfCode,
+	};
+	Result _compile_line(void);
 	// test if a rule is matching the current stack
-	bool is_rule_matching_stack(RuleDefinition& rule) const;
+	bool is_rule_matching_stack(GrammarRule& rule) ;
 	// find a matchin rule from the token stack
-	RuleDefinition* _find_matching_rule_from_stack(void);
+	GrammarRule* _find_matching_rule_from_stack(void);
 	// call a rule and remove the token from the stack
-	void _execute_rule(RuleDefinition& rule);
+	void _execute_rule(GrammarRule& rule);
 };
 
 
